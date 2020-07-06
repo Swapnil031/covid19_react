7648eb92d211e3dd20e2bec3d2265eab899ea0d5
Swapnil031 <swapnilaudichya@gmail.com>
2020-07-06 16:55:00 +0530

    Updates

---
+++ precache-manifest.d35f0994a48b37421f3494202586b3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb2d73ec1cbd6fbbb7e43e2cedb9d13",
+    "url": "/covid19_react/index.html"
+  },
+  {
+    "revision": "b4d785910e5e114255ae",
+    "url": "/covid19_react/static/css/main.d225f469.chunk.css"
+  },
+  {
+    "revision": "781b67cdbfb7e038a989",
+    "url": "/covid19_react/static/js/2.52c2f465.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_react/static/js/2.52c2f465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d785910e5e114255ae",
+    "url": "/covid19_react/static/js/main.d8b0b196.chunk.js"
+  },
+  {
+    "revision": "54a59406baf366ebce87",
+    "url": "/covid19_react/static/js/runtime-main.0c93691f.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19_react/static/media/coronaImage.d7265326.png"
+  }
+]);
